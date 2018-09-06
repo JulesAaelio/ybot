21b95b6b20212703a4a8d68b2a6c6182d35253fc
Jules LAURENT <jules.laurent@ynov.com>
2018-09-06 12:14:27 +0200

    make bot send guidelines when receive github payload wip

--- src/features/update-guidelines.js
+++ src/features/update-guidelines.js
@@ -1,8 +1,23 @@
+const request = require('../utils/json-request');
 module.exports = (bot, app) => {
-    app.post('/guidelines',(req,res) => {
-        if(req.body.ref && req.body.ref === process.env.guidelines_tracked_ref) {
+    app.post('/guidelines', (req, res) => {
+        if (req.body.ref && req.body.ref === process.env.guidelines_tracked_ref) {
             console.log('guidelines master updated');
+            fetchAllGuidelines(req.body.repository.contents_url.replace('/{+path}', '')).then(guidelines => {
+                bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['RULES.md']}`);
+                bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['MANIFEST.md']}`);
+                bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['NEWCOMERS_GUIDE.md']}`);
+            })
         }
         res.status(200).send();
     });
 };
+
+async function fetchAllGuidelines(url) {
+    const guidelinesList = JSON.parse(await request(url));
+    const guidelines = {};
+    for(let file of guidelinesList) {
+        guidelines[file.name] = await request(file.download_url);
+    }
+    return guidelines;
+}

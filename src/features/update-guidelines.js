a4757945e4097c4a32be4264bade35eb13d0b709
Jules LAURENT <jules.laurent@ynov.com>
2018-09-06 12:36:09 +0200

    fix payload verification condition position

--- src/features/update-guidelines.js
+++ src/features/update-guidelines.js
@@ -1,24 +1,25 @@
 const request = require('../utils/json-request');
 module.exports = (bot, app) => {
     app.post('/guidelines', (req, res) => {
-        flushChannel(bot.channels.get(process.env.guidelines_channel_id)).then(() => {
-            if (req.body.ref && req.body.ref === process.env.guidelines_tracked_ref) {
+        if (req.body.ref && req.body.ref === process.env.guidelines_tracked_ref) {
+            flushChannel(bot.channels.get(process.env.guidelines_channel_id)).then(() => {
+
                 console.log('guidelines master updated');
                 fetchAllGuidelines(req.body.repository.contents_url.replace('/{+path}', '')).then(guidelines => {
                     bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['RULES.md']}`);
                     bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['MANIFEST.md']}`);
                     bot.channels.get(process.env.guidelines_channel_id).send(`@everyone\n\n ${guidelines['NEWCOMERS_GUIDE.md']}`);
-                })
-            }
-            res.status(200).send();
-        })
+                });
+                res.status(200).send();
+            })
+        }
     });
 };
 
 async function fetchAllGuidelines(url) {
     const guidelinesList = JSON.parse(await request(url));
     const guidelines = {};
-    for(let file of guidelinesList) {
+    for (let file of guidelinesList) {
         guidelines[file.name] = await request(file.download_url);
     }
     return guidelines;
@@ -27,6 +28,6 @@ async function fetchAllGuidelines(url) {
 async function flushChannel(channel) {
     const messages = await channel.fetchMessages();
     messages.forEach((message) => {
-       message.delete();
+        message.delete();
     });
 }

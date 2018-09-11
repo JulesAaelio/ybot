bf8ae7a91b3151ddab1950712dbe54c957f70c38
Jules LAURENT <jules.laurent@ynov.com>
2018-09-11 21:28:38 +0200

    issue a jwt token on validation request

--- src/features/send-confirmation.js
+++ src/features/send-confirmation.js
@@ -1,11 +1,12 @@
 const nodemailer = require('nodemailer');
 const pug = require('pug');
 const mailRegexp = /^(.*)@ynov.com$/;
+const jwt = require('jsonwebtoken');
 
 const transporter = nodemailer.createTransport({
     host: process.env.smtp_host,
     port: 465,
-    auth : {
+    auth: {
         user: process.env.smtp_user,
         pass: process.env.smtp_password,
     }
@@ -15,33 +16,60 @@ async function bla() {
 
 }
 
-async function sendConfirmationEmail(to, link) {
+async function sendConfirmationEmail(to, pseudo, token) {
     return transporter.sendMail({
         from: "YBot - Discord Ynov Lyon - <discord-ynov-lyon@outlook.com>",
         to: to,
         subject: 'Confirme ton adresse mail ! ',
-        html : pug.renderFile('src/templates/mail.pug',  {
-                pseudo: 'Jules',
-                link: link,
+        html: pug.renderFile('src/templates/mail.pug', {
+                pseudo: pseudo,
+                link: `${process.env.hostname}/activate?token=${token}`,
                 image: process.env.hostname + '/images/logo.png'
             }
         )
     })
 }
 
-module.exports = (bot, app, db) => {
-  bot.on('message', (message) => {
-      if(message.channel.id === process.env.newcomers_channel_id && message.author.id !== bot.user.id) {
-          if(mailRegexp.test(message.content)) {
+async function storeValidation(db, message) {
+    const validation = await db.Validation.findOne({
+        where: {
+            userId: message.author.id,
+            guildId: message.guild.id,
+        }
+    });
 
-                db.Validation.create({
-                    email : message.content,
+    if (validation) {
+        await validation.update({
+            email: message.content,
+        })
+    } else {
+        await db.Validation.create({
+            userId: message.author.id,
+            guildId: message.guild.id,
+            email: message.content,
+        })
+    }
+}
+
+module.exports = (bot, app, db) => {
+    bot.on('message', async (message) => {
+        if (message.channel.id === process.env.newcomers_channel_id && message.author.id !== bot.user.id) {
+            if (mailRegexp.test(message.content)) {
+
+                await storeValidation(db,message);
+
+                const token = jwt.sign({
                     userId: message.author.id,
-                    guildId : message.guild.id,
-                });
-          }else {
-              message.reply('please provide a valid academic address');
-          }
-      }
-  })
+                    guildId: message.guild.id,
+                    email: message.content,
+                }, process.env.jwt_secret);
+
+                await sendConfirmationEmail(message.content, message.author.username, token);
+
+                message.reply('A confirmation email has been sent');
+            } else {
+                message.reply('please provide a valid academic address');
+            }
+        }
+    })
 };

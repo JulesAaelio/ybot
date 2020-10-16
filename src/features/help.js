2ed13dc114e90bb6b4f02c6e5cab3b70eac39fea
Jules LAURENT <jules.laurent@ynov.com>
2020-10-16 12:56:46 +0200

    Add carriage returns to help

--- src/features/help.js
+++ src/features/help.js
@@ -6,7 +6,7 @@ module.exports = (bot, app, db) => {
             .setTitle('Ybot - Commandes disponibles ')
             .setColor(0xff0000)
             .setDescription('\`!y help\` : Show help message\n' +
-                '\`!y trombi whois @someone\`\` : Show picture of user if registered\n' +
+                '\`!y trombi whois @someone\` : Show picture of user if registered\n' +
                 '\`!y trombi set-picture <link>\` : Set your picture, you can also add an attachment\n' +
                 '\`!y nickname <nickname>\` : Change your nickname\n' +
                 '\`!y validate <youradress@ynov.com>\` : Send confirmation email\n');
@@ -18,7 +18,7 @@ module.exports = (bot, app, db) => {
             const adminEmbed =  new MessageEmbed()
                 .setTitle('Ybot - Commandes disponibles ')
                 .setColor(0xff0000)
-                .setDescription('**!y roles <emoji:@role> <emoji:@role> ...** : Send message with reactions to get roles ' );
+                .setDescription(' \`!y roles <emoji:@role> <emoji:@role> ...\`: Send message with reactions to get roles' );
             message.reply(adminEmbed);
         }
     };
